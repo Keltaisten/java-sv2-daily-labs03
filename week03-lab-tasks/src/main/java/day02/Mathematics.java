bf2da764518be7d24668c839077a94ae059096d3
Keltaisten <keltaisten@gmail.com>
2021-10-26 20:05:00 +0200

    day02

---
+++ week03-lab-tasks/src/main/java/day02/Mathematics.java
@@ -0,0 +1,22 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Mathematics {
+
+    public boolean isPrime(int number){
+        for(int i = 2; i<number;i++){
+            if(number%i == 0){
+               return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        Mathematics mathematics = new Mathematics();
+        System.out.println("Kérem adjon meg egy számot.");
+        System.out.println("A szám prím? " + mathematics.isPrime(scann.nextInt()));
+    }
+}
