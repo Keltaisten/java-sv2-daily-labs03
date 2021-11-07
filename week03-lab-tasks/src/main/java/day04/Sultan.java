8ce516b5a233dc4bee50c81f62fe4d6e606089f0
Keltaisten <keltaisten@gmail.com>
2021-11-07 21:51:48 +0100

    PrisonBreak

--- week03-lab-tasks/src/main/java/day04/Sultan.java
+++ week03-lab-tasks/src/main/java/day04/Sultan.java
@@ -1,11 +1,9 @@
 package day04;
 
-import java.util.Arrays;
-
 public class Sultan {
     public static void main(String[] args) {
         Prison prisonBreak = new Prison();
-        prisonBreak.openFreeCells();;
+        prisonBreak.openFreeCells();
         System.out.println(prisonBreak.openedDoors);
     }
 }
